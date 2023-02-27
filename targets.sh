529b6af6504dae838ccb6c8302b2b65cab4705bc
Paul Schulz <paul@mawsonlakes.org>
2023-02-27 14:18:45 +1030

    Modifies code to allow it to be built with OpenSSL 3.0
    The current version of OpenSSL available on Ubuntu 22.10 is 3.0.5.

--- targets.sh
+++ targets.sh
@@ -18,10 +18,10 @@ if [[ $tls_lib == "openssl" ]]; then
 	exit 0
     fi
     version=`openssl version -a`
-    if [[ ! ($version =~ "OpenSSL 1.1.0") ]]; then
-	echo "The EPRI 2030.5 client requires OpenSSL version 1.1.0."
-	echo "openssl version -a"
-	echo $version
+    if [[ ! ($version =~ "OpenSSL 3.0.") ]]; then
+	    echo "The EPRI 2030.5 client requires OpenSSL version 3.0."
+	    echo "openssl version -a"
+	    echo $version
 	exit 0
     fi
 else
